8993a243da592cb6cb6108a632e8aa9f0abf6386
GabLipid01 <acessogwjp@gmail.com>
2025-05-05 20:20:11 -0300

    Add files via upload

---
+++ sensory_esg_data.py
@@ -0,0 +1,21 @@
+dados_sensoriais = {
+    "Palma": {
+        "notas": ["amendoado", "terroso"],
+        "emoções": ["conforto", "nutrição"]
+    },
+    "Palmiste": {
+        "notas": ["coco", "doce"],
+        "emoções": ["alegria", "leveza"]
+    }
+}
+
+dados_esg = {
+    "Palma": {
+        "emissões_CO2": 1.5,
+        "uso_água": 1200
+    },
+    "Palmiste": {
+        "emissões_CO2": 1.1,
+        "uso_água": 950
+    }
+}
