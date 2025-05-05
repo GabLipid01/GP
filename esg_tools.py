0e0fe7314d55703c3a1ff42a03582cfd70a87450
GabLipid01 <acessogwjp@gmail.com>
2025-05-05 20:31:15 -0300

    Delete esg_tools.py

--- esg_tools.py
+++
@@ -1,6 +0,0 @@
-def calcular_esg(blend, dados_esg):
-    impacto = {"CO2_total": 0, "água_total": 0}
-    for oleo, percentual in blend.items():
-        impacto["CO2_total"] += dados_esg[oleo]["emissões_CO2"] * percentual / 100
-        impacto["água_total"] += dados_esg[oleo]["uso_água"] * percentual / 100
-    return impacto
