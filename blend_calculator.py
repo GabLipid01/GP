e78e8b3b4968644815818b6ae77451775b61f2cd
GabLipid01 <acessogwjp@gmail.com>
2025-05-05 20:29:19 -0300

    Delete blend_calculator.py

--- blend_calculator.py
+++
@@ -1,9 +0,0 @@
-def calcular_blend_otimo(oleo_base, teor_oleico_desejado):
-    return {
-        "Óleo base": oleo_base,
-        "Teor de ácido oleico desejado": teor_oleico_desejado,
-        "Blend final": {
-            "Palma": 60,
-            "Palmiste": 40
-        }
-    }
